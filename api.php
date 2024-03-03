c5da570ba87e8dcc4b3623722d34eb72bc281590
SouthernMinami <kanou373@gmail.com>
2024-03-03 09:26:31 +0900

    Fix: 全ポートからの通信を許可

--- api.php
+++ api.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/vendor/autoload.php";
 use function Jawira\PlantUml\encodep;
 
-header("Access-Control-Allow-Origin: http://localhost:8000");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 
