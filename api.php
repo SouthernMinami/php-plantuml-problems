cd8446a19102daeea0b5f1379c1334a604cb7756
SouthernMinami <kanou373@gmail.com>
2024-06-08 13:33:59 +0900

    Fix: Content-Typeのheader()

--- api.php
+++ api.php
@@ -4,6 +4,7 @@ require_once __DIR__ . "/vendor/autoload.php";
 use function Jawira\PlantUml\encodep;
 
 header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json");
 
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
     ini_set('display_errors', 1);
